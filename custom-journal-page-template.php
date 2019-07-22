fe73532fcad6763c45da4ee0eb7aedef0c1dfdf9
Tom Pearson <tom.g.pearson@gmail.com>
2019-07-22 12:45:54 +0100

    add posts & structure to journal page

--- custom-journal-page-template.php
+++ custom-journal-page-template.php
@@ -3,5 +3,36 @@ global $pageTag;
 $pageTag = 'journal';
 ?>
 <?php get_template_part('partials/html','top'); ?>
-JOURNAL
+<?php /* the top journal entry */ ?>
+<div class="atf-grid journal-splash">
+  <?php 
+  $posts = get_category_post_range('journal',0,1);
+  while ( $posts->have_posts() ) : $posts->the_post(); ?>
+    <div class="journal-splash__text">
+      <div class="journal-splash__kicker">The latest</div>
+      <h2 class="journal-splash__title"><?php the_title(); ?></h2>						
+      <div class="journal-splash__date"><?php the_date() ?></div>
+      <div class="journal-splash__excerpt"><?php the_excerpt(); ?></div>
+      <a href="<?php the_permalink(); ?>" class="journal-splash__link">Read the journal post</a>
+    </div>
+    <div class="journal-splash__image">
+    </div>
+  <?php endwhile; ?>
+</div>
+<?php /* the remaining journal entries */ ?>
+<div class="atf-grid journal-archive">
+  <div>Recently</div>
+  <div class="archive-subgrid">
+  <?php 
+  $posts = get_category_post_range('journal', 1, 9);
+  while ( $posts->have_posts() ) : $posts->the_post(); ?>
+  <div>
+    <div class="journal-archive__date"><?php the_date() ?></div>
+    <h2 class="journal-archive__title"><?php the_title(); ?></h2>						
+    <div class="journal-archive__excerpt"><?php the_excerpt(); ?></div>
+    <a href="<?php the_permalink(); ?>" class="journal-archive__link">Continue reading</a>
+  </div>
+  <?php endwhile; ?>
+  </div>
+</div>
 <?php get_template_part('partials/html','bottom'); ?>
