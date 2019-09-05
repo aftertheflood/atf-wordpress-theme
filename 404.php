d5e1373faa5b7a7b3ce24d5b5e1d43d57616f06e
Tom P <tom@aftertheflood.com>
2019-09-05 17:00:41 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Client Page Template */ 
+global $pageTag;
+?>
+<?php get_template_part('partials/html','top'); ?>
+<div class="error">
+    <p>404: Sorry, this page doesn't exist.</p>
+</div>
+<?php get_template_part('partials/html','bottom'); ?>
