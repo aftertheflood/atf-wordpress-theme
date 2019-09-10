d964fc11a5cada46c783edb2410ddcf787212079
Tom Pearson <tom.g.pearson@gmail.com>
2019-09-10 14:10:06 +0100

    start of an article grid

--- index.php
+++ index.php
@@ -7,14 +7,20 @@
 	<?php if ( have_posts() ) :?>
 		<?php while ( have_posts() ) : the_post(); ?>
 		<div class="post">
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>						
+			
+								
 			<?php
-				if ( is_singular() ){ 
-					the_content(); 
-				}else{ 
-					the_excerpt();
-				}
-			?>
+				if ( is_singular() ){ ?>
+				<div class="article-head">
+					<h2><?php the_title(); ?></h2>
+				</div>
+				<div class="article-body atf-grid">
+					<?php the_content(); ?>
+				</div>
+					<?php }else{ ?> 
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<?php the_excerpt();
+				} ?>
 		</div>
 		<?php endwhile; ?>
 	<?php else : ?>
