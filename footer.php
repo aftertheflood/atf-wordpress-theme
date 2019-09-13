ba9c0b97a75d5df97ad1541ae7f7cb1fd264fd43
Tom P <tom@aftertheflood.com>
2019-09-13 10:37:51 +0100

    improve footer styling

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
-<footer>
+<footer class="atf-footer">
   <h2>Would you like to know more?</h2>
-  <div class="contact-link">
-    <a href="<?php echo esc_url( home_url( '/contact' ) ); ?>">Contact</a>
-  </div>
+  <div class="footer-contact-link">
+    <a href="<?php echo esc_url( home_url( '/contact' ) ); ?>" > 
+      Get in touch
+    </a>
+</div>
   <div class="address">
     <span class="address-highlight">After the flood</span><br>
     140-142 St John St, London, EC1V 4UB, UK<br>
     info@aftertheflood.com<br>
-    @afterthefloodco<br>
+    <a href="https://twitter.com/AftertheFloodco">@afterthefloodco</a><br>
     Registered in England and Wales as After the flood Limited. 7792366
   </div>
 </footer>
