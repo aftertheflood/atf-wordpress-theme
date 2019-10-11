5427349484ccba2cab9020d1f479da6911be9675
michaelgallagher <mail@mikegallagher.org>
2019-10-11 15:05:57 +0100

    revise the footer type and links

--- footer.php
+++ footer.php
@@ -1,15 +1,23 @@
-<footer class="atf-footer">
-  <h2>Would you like to know more?</h2>
-  <div class="footer-contact-link">
-    <a href="<?php echo esc_url( home_url( '/contact' ) ); ?>" > 
-      Get in touch
-    </a>
-</div>
-  <div class="address">
-    <span class="address-highlight">After the flood</span><br>
-    140-142 St John St, London, EC1V 4UB, UK<br>
-    info@aftertheflood.com<br>
-    <a href="https://twitter.com/AftertheFloodco">@afterthefloodco</a><br>
-    Registered in England and Wales as After the flood Limited. 7792366
-  </div>
+<footer class="atf-footer atf-grid">
+	<h2>Would you like to know more?</h2>
+	<div class="footer-contact-link">
+		<a href="<?php echo esc_url( home_url( '/contact' ) ); ?>" > 
+			Get in touch
+		</a>
+	</div>
+	<div class="social">
+		<a href="https://twitter.com/afterthefloodco?lang=en"><img src="wp-content/themes/atf-wordpress-theme/assets/images/twitter-social-circle-white.svg" class="social-icon"></a>
+		<a href="https://www.instagram.com/afterthefloodco/"><img src="wp-content/themes/atf-wordpress-theme/assets/images/instagram-social-circle-white.svg" class="social-icon"></a>
+		<a href="https://medium.com/@AftertheFloodco"><img src="wp-content/themes/atf-wordpress-theme/assets/images/medium-social-circle-white.svg" class="social-icon"></a>
+		<a href="https://www.linkedin.com/company/after-the-flood"><img src="wp-content/themes/atf-wordpress-theme/assets/images/linkedin-social-circle-white.svg" class="social-icon"></a>
+	</div>
+	<div class="address">
+		<span class="address-highlight">After the flood</span><br>
+		140-142 St John St, London, EC1V 4UB, UK<br>
+		info@aftertheflood.com<br>
+		Registered in England and Wales as After the flood Limited. 7792366
+	</div>
+	<div class="footer-logo">
+		<img src="wp-content/themes/atf-wordpress-theme/assets/images/atf-logo-clipped-footer.png">
+	</div>
 </footer>
