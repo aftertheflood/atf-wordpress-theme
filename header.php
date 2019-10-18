4f6bb6f27362aee4b5500c1593976474def3a7e4
michaelgallagher <mail@mikegallagher.org>
2019-10-18 15:07:08 +0100

    fix the mobile navigation

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
       </div>
     </div>
     <div class="header-links">
-      <div class="header-link">
+      <div class="header-button">
         <a class="menu-link open-menu-button" href="#compact-site-menu">
         <svg width="36" height="36" viewBox="0 0 36 36" fill="none">
           <circle cx="18" cy="18" r="18" fill="black"/>
