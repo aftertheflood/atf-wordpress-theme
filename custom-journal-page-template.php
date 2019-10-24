bae8ad6a96fdc0535edb7f1a92696cd14b2f8295
michaelgallagher <mail@mikegallagher.org>
2019-10-24 14:19:11 +0100

    adjustments for the journal splash when there is no image

--- custom-journal-page-template.php
+++ custom-journal-page-template.php
@@ -8,34 +8,40 @@ $paged = get_query_var( 'paged' ); // what page of the archive etc are we on
 if($paged == 0){
 ?>
 
-<div class="atf-grid journal-splash">
-  <?php 
-  $posts = get_category_post_range('journal',0,1);
-  while ( $posts->have_posts() ) : $posts->the_post(); ?>
-    <?php if(get_post_meta($post->ID, 'promo-image', true)) { ?>  
-    <div class="journal-splash__text">
-      <div class="journal-splash__kicker">The latest</div>
-      <h2 class="journal-splash__title"><?php the_title(); ?></h2>
-      <div class="journal-splash__subtitle"><?php echo get_post_meta($post->ID, 'subtitle', true); ?></div>			
-      <div class="journal-splash__date"><?php the_date() ?></div>
-      <div class="journal-splash__excerpt"><?php the_excerpt(); ?></div>
-      <a href="<?php the_permalink(); ?>" class="journal-splash__link">Read more</a>
+<?php 
+$posts = get_category_post_range('journal',0,1);
+while ( $posts->have_posts() ) : $posts->the_post(); ?>
+  <?php if(get_post_meta($post->ID, 'promo-image', true)) { ?>  
+    <div class="atf-grid journal-splash">
+      <div class="journal-splash__text">
+        <div class="journal-splash__kicker">The latest</div>
+        <h2 class="journal-splash__title"><?php the_title(); ?></h2>
+        <div class="journal-splash__subtitle"><?php echo get_post_meta($post->ID, 'subtitle', true); ?></div>			
+        <div class="journal-splash__date"><?php the_date() ?></div>
+        <div class="journal-splash__excerpt"><?php the_excerpt(); ?></div>
+        <a href="<?php the_permalink(); ?>" class="journal-splash__link">Read more</a>
+      </div>
+      <div class="journal-splash__image" style="background-image: url(<?php echo get_post_meta($post->ID, 'promo-image', true);?>)">
+        <img src="<?php echo get_post_meta($post->ID, 'promo-image', true);?>">
+      </div>
     </div>
-    <div class="journal-splash__image" style="background-image: url(<?php echo get_post_meta($post->ID, 'promo-image', true);?>)">
-      <img src="<?php echo get_post_meta($post->ID, 'promo-image', true);?>">
+  <?php } else { ?>
+    <div class="atf-grid journal-splash__textonly">
+      <div class="journal-splash__text">
+        <div class="journal-splash__kicker">The latest</div>
+        <h2 class="journal-splash__title"><?php the_title(); ?></h2>
+        <div class="journal-splash__subtitle"><?php echo get_post_meta($post->ID, 'subtitle', true); ?></div>			
+        <div class="journal-splash__date"><?php the_date() ?></div>
+        <div class="journal-splash__excerpt"><?php the_excerpt(); ?></div>
+        <a href="<?php the_permalink(); ?>" class="journal-splash__link">Read more</a>
+      </div>
     </div>
-    <?php } else { ?>
-    <div class="journal-splash__textonly">
-      <div class="journal-splash__kicker">The latest</div>
-      <h2 class="journal-splash__title"><?php the_title(); ?></h2>
-      <div class="journal-splash__subtitle"><?php echo get_post_meta($post->ID, 'subtitle', true); ?></div>			
-      <div class="journal-splash__date"><?php the_date() ?></div>
-      <div class="journal-splash__excerpt"><?php the_excerpt(); ?></div>
-      <a href="<?php the_permalink(); ?>" class="journal-splash__link">Read more</a>
-    </div>
-    <?php } ?>
-  <?php endwhile; ?>
-</div>
+  <?php } ?>
+<?php endwhile; ?>
+
+
+
+
 <?php } /* the remaining journal entries */ ?>
 <div class="atf-grid journal-archive">
   <?php if($paged == 0){ ?>
