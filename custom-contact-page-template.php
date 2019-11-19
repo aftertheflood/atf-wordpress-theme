5760b3b1deff8fe39f9a9cd32b094b8681770c06
michaelgallagher <mail@mikegallagher.org>
2019-11-19 12:10:14 +0000

    begin revising contact form layout to accommodate more info

--- custom-contact-page-template.php
+++ custom-contact-page-template.php
@@ -4,12 +4,21 @@ $pageTag = 'contact';
 ?>
 <?php get_template_part('partials/html','top'); ?>
 <div class="contact__content">
-  <h1><?php the_title(); ?></h1>
-  <?php if ( have_posts() ) :?>
-    <?php while ( have_posts() ) : the_post(); ?>
-    <?php the_content(); ?>
-    <?php endwhile; ?>
-  <?php endif; ?>
+  <div class="contact__info">
+    <h1><?php the_title(); ?></h1>
+    <div class="the-contact-details">
+      <p><strong>Phone:</strong> +44 (0)23 5678 9012</p>
+      <p><strong>Email:</strong> info@aftertheflood.com</p>
+    </div>
+    <div class="the-map"></div>
+  </div>
+  <div class="contact__form">
+    <?php if ( have_posts() ) :?>
+      <?php while ( have_posts() ) : the_post(); ?>
+      <?php the_content(); ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div>
 </div>
 
 <?php get_template_part('partials/html','bottom'); ?>
